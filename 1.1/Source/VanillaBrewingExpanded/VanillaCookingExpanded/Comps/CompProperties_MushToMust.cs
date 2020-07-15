7272f3ca3f476fb7aceeed33d5bf85a5a0a61457
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-07-15 09:56:48 +0200

    v0.1

---
+++ 1.1/Source/VanillaBrewingExpanded/VanillaCookingExpanded/Comps/CompProperties_MushToMust.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaBrewingExpanded
+{
+    public class CompProperties_MushToMust : CompProperties
+    {
+        public int TicksToRotStart
+        {
+            get
+            {
+                return Mathf.RoundToInt(this.daysToRotStart * 60000f);
+            }
+        }
+
+        public int TicksToDessicated
+        {
+            get
+            {
+                return Mathf.RoundToInt(this.daysToDessicated * 60000f);
+            }
+        }
+
+        public CompProperties_MushToMust()
+        {
+            this.compClass = typeof(CompMushToMust);
+        }
+
+        public CompProperties_MushToMust(float daysToRotStart)
+        {
+            this.daysToRotStart = daysToRotStart;
+        }
+
+
+
+        public float daysToRotStart = 2f;
+
+        public bool rotDestroys;
+
+        public float rotDamagePerDay = 40f;
+
+        public float daysToDessicated = 999f;
+
+        public float dessicatedDamagePerDay;
+
+        public bool disableIfHatcher;
+    }
+}
