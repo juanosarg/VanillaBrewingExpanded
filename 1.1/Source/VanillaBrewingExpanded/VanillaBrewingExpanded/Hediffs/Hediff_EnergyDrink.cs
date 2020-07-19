cb36d32d97e9aa059681bd9ac8121e197be29b9a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-07-19 16:42:50 +0200

    v0.1

---
+++ 1.1/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Hediffs/Hediff_EnergyDrink.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaBrewingExpanded
+{
+    public class Hediff_EnergyDrink : Hediff_High
+    {
+
+       
+
+        public override void PostRemoved()
+        {
+            base.PostRemoved();
+            Hediff hediff = this.pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named("VBE_EnergyDrinkCrash"), false);
+            if (hediff != null)
+            {
+                hediff.Severity = 1f;
+                return;
+            }
+            hediff = HediffMaker.MakeHediff(HediffDef.Named("VBE_EnergyDrinkCrash"), this.pawn, null);
+            hediff.Severity = 1f;
+            this.pawn.health.AddHediff(hediff, null, null, null);
+        }
+
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+            this.pawn.needs.rest.CurLevel += 0.2f;
+        }
+    }
+}
+
---
+++ 1.1/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Hediffs/Hediff_EnergyDrinkHangover.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaBrewingExpanded
+{
+    public class Hediff_EnergyDrinkHangover : HediffWithComps
+    {
+        public override bool Visible
+        {
+            get
+            {
+                return !this.pawn.health.hediffSet.HasHediff(HediffDef.Named("VBE_EnergyDrinkCrash"), false) && base.Visible;
+            }
+        }
+
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+            this.pawn.needs.rest.CurLevel -= 0.2f;
+        }
+    }
+}
