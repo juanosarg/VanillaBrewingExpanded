c3998e598e652d94282f5d7e3e4e463c7e880813
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-07-22 19:04:51 +0200

    v0.1

---
+++ 1.1/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Hediffs/Hediff_CiderRemoveToxic.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaBrewingExpanded
+{
+    public class Hediff_CiderRemoveToxic : HediffWithComps
+    {
+
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+            Hediff hediff = this.pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.ToxicBuildup, false);
+            if (hediff != null)
+            {
+                hediff.Severity -= 0.2f;
+                return;
+            }
+
+        }
+    }
+}
