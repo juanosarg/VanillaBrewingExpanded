fcaaac17e677f69decdd01999e9f18f0ed03501b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-11-04 11:18:29 +0100

    v1.105

--- 1.2/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Plants/Plant_AutoProduce.cs
+++ 1.2/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Plants/Plant_AutoProduce.cs
@@ -15,15 +15,16 @@ namespace VanillaBrewingExpanded
         public override void Tick()
         {
             base.Tick();
-            if (this.IsHashIntervalTick(60000) && this.Growth>0.7) {
+            if (this.IsHashIntervalTick(120000) && this.Growth>0.7) {
                 System.Random random = new System.Random();
                 if (random.NextDouble() > 0.4) {
                     Thing thing = ThingMaker.MakeThing(this.def.plant.harvestedThingDef, null);
+                    thing.SetForbidden(true);
                     if (random.NextDouble() < 0.4)
                     {
-                        thing.stackCount = 1;
-                    } else {
                         thing.stackCount = 2;
+                    } else {
+                        thing.stackCount = 4;
                     }                       
                     GenPlace.TryPlaceThing(thing, this.Position+IntVec3.South, this.Map, ThingPlaceMode.Near, null, null, default(Rot4));
                 }
